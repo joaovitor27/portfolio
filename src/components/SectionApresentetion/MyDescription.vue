9a769999cbf34052bfbb862530268123f79c82f1
João Vitor <58125095+joaovitor27@users.noreply.github.com>
2024-10-16 10:25:42 -0300

    Update MyDescription.vue

--- src/components/SectionApresentetion/MyDescription.vue
+++ src/components/SectionApresentetion/MyDescription.vue
@@ -3,11 +3,11 @@
     <h3 class="title is-2">Sobre mim</h3>
     <h4 class="subtitle is-5">João Vitor, "Rei da programação"</h4>
     <div class="container has-text-justified">
-      <p>Sou desenvolvedor a <strong>5 anos</strong>, e durante esses anos vim adquirindo cada vez mais
+      <p>Sou desenvolvedor a <strong>6 anos</strong>, e durante esses anos vim adquirindo cada vez mais
         experiência como programador,
         gosto mais de trabalhar na parte do back end, mas já fiz alguns trabalho com front-end como esse portfólio.
-        Atualmente estou cursando
-        <strong>ciência da computação</strong>, no último período e sou desenvolvedor Back-end na startup RevGás.
+        Atualmente sou formado em
+        <strong>ciência da computação</strong> e sou desenvolvedor Back-end na startup RevGás.
       </p>
     </div>
   </div>
